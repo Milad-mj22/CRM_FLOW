7dbeb03ee55f495fc44300f7efade52ef9bf975e
milad <m.moltaji@yahoo.com>
2025-08-04 16:19:05 +0330

    right categories

--- utils/urls.py
+++ utils/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import import_buyers_csv, ticket_create, ticket_detail, ticket_list
+from .views import  import_buyers_csv, ticket_create, ticket_detail, ticket_list
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,7 +11,6 @@ urlpatterns = [
     path('tickets/', ticket_list, name='ticket_list'),
     path('tickets/create/', ticket_create, name='ticket_create'),
     path('tickets/<int:ticket_id>/', ticket_detail, name='ticket_detail'),
-
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
